d2818a0eef5a6af34989efa84f6457abea979570
Sanjay Soundarajan <sanjay.16@live.com>
2024-01-13 21:58:00 -0800

    chore: Update mermaid-cli version and create output folder

--- scripts/generate-diagrams.mjs
+++ scripts/generate-diagrams.mjs
@@ -1,12 +1,16 @@
-import chokidar from 'chokidar';
-import path from 'path';
-import { run } from '@mermaid-js/mermaid-cli';
-import fs from 'fs';
+import { run } from "@mermaid-js/mermaid-cli";
+import fs from "fs";
+import path from "path";
 
 const root_folder = path.resolve();
 
-const mermaidInputPath = path.join(root_folder, 'docs', 'mermaid');
-const mermaidOutputPath = path.join(root_folder, 'docs', 'public', 'mermaid');
+const mermaidInputPath = path.join(root_folder, "docs", "mermaid");
+const mermaidOutputPath = path.join(root_folder, "docs", "public", "mermaid" );
+
+// create the output folder if it doesn't exist
+if (!fs.existsSync(mermaidOutputPath)) {
+  fs.mkdirSync(mermaidOutputPath);
+}
 
 const generateMermaidGraph = async (inputPath) => {
   const fileName = path.parse(inputPath).name;
@@ -16,43 +20,15 @@ const generateMermaidGraph = async (inputPath) => {
   console.log(`Generating ${outputPath}`);
 
   await run(inputPath, outputPath, {
-    outputFormat: 'svg',
+    outputFormat: "svg",
   });
 
   console.log(`Generated ${outputPath}`);
 };
 
-if (process.argv[2] && process.argv[2] === '-d') {
-  // watch for changes in the mermaid input folder
-  console.log(`Watching ${mermaidInputPath}`);
+// loop through all files in the mermaid input folder
+fs.readdirSync(mermaidInputPath).forEach(async (file) => {
+  const inputPath = path.join(mermaidInputPath, file);
 
-  const watcher = chokidar.watch(mermaidInputPath, {
-    ignored: /(^|[\/\\])\../,
-    persistent: true,
-  });
-
-  watcher.on('add', async (path) => {
-    console.log(`File ${path} has been added`);
-
-    await generateMermaidGraph(path);
-  });
-
-  watcher.on('change', async (path) => {
-    console.log(`File ${path} has been changed`);
-
-    await generateMermaidGraph(path);
-
-    // trigger a reload of the page
-    if (import.meta.hot) {
-      import.meta.hot.send('reload');
-    }
-  });
-} else {
-  // loop through all files in the mermaid input folder
-
-  fs.readdirSync(mermaidInputPath).forEach(async (file) => {
-    const inputPath = path.join(mermaidInputPath, file);
-
-    await generateMermaidGraph(inputPath);
-  });
-}
+  await generateMermaidGraph(inputPath);
+});
